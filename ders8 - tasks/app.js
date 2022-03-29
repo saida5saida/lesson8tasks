a34788a83236119d210f7b0c25197061855278e0
saida5saida <98975175+saida5saida@users.noreply.github.com>
2022-03-29 15:49:55 +0400

    Add files via upload

---
+++ ders8 - tasks/app.js
@@ -0,0 +1,71 @@
+// Sual 1 
+
+// let array1 = ['kitab', 'defter', 'qelem', 'pozan']
+// array1.splice(2, 1, "jurnal", "karandas")
+// console.log(array1)
+
+// Sual 2 
+
+// const array2 = [2, 5, 32, 12, 6] 
+// console.log(array2.every((reqem) => {
+//  return reqem%2 === 0
+// }))
+
+// console.log(array2.some((reqem)=> {
+//     return reqem%2===1
+// }
+// ))
+
+// Sual 3
+
+
+// const array3 = [-23, 5, 34, 3, -3, 0]
+// console.log(array3.filter((number) => {
+// return number >0
+// }))
+// let newAr = [5, 34, 3]
+// newAr.sort(function (a,b) {
+//     return a-b;
+// })
+// console.log(newAr)
+
+
+
+// Sual 4
+
+
+// let duyme = 1
+// switch(duyme) {
+//     case 1:
+//         duyme = "save"
+//         console.log("yadda saxlanildi")
+//         break
+//     case 2:
+//         duyme = "cancel"
+//         console.log("cixarildi")
+//         break
+//     case 3:
+//         duyme = "update"
+//         console.log("deyisiklikler yadda saxlanildi")
+//         break
+//     default:
+//     console.log("qebul edilmedi")
+// }
+
+// Sual 6
+
+// let array6 = Array.from("abcfdeg");
+// console.log(array6)
+
+
+// Sual 7 
+
+// const object7 = {cloud: 'bulud', book: 'kitab', juice: 'meyve siresi'} 
+// const object8 = {phone: 'telefon', window: 'pencere'}
+// const object9 = Object.assign({}, object7, object8)
+// console.log(object9)
+
+// console.log(Object.values(object9))
+
+
+
